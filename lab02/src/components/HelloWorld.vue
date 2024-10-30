8f6e32cdc82f8b6beda39bb71247a086579c52f7
DaiYic <D2998654493@outlook.com>
2024-10-30 14:07:48 +0800

    1

---
+++ lab02/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="greetings"></div>
+</template>
+
+<script setup lang="ts">
+// 这里可以保留或删除 defineProps 的部分，视你的需求而定
+</script>
+
+<style scoped>
+/* 可以保持为空，或者删除整个 <style> 标签 */
+</style>
